4c322eccfaf2c35fb979ab15d4940026c5fcdf46
Maksim Laptenko <laptenko0811@icloud.com>
2021-04-19 14:04:15 +0300

    вынес из цикла расчет среднего. Убрал static из методов. В Тестах снова создается новый сервис.

--- src/main/java/ru/netology/StatsService.java
+++ src/main/java/ru/netology/StatsService.java
@@ -2,7 +2,7 @@ package ru.netology;
 
 public class StatsService {
 
-    public static int sum(int[] sales) { // сумма всех продаж
+    public int sum(int[] sales) { // сумма всех продаж
         int sum = 0;
         for (int sale : sales) {
             sum += sale;
@@ -10,12 +10,12 @@ public class StatsService {
         return sum;
     }
 
-    public static int average(int[] sales) { // средняя сумма продаж в месяц
-        int average = StatsService.sum(sales) / sales.length;
+    public int average(int[] sales) { // средняя сумма продаж в месяц
+        int average = sum(sales) / sales.length;
         return average;
     }
 
-    public static int monthMaxSale(int[] sales) { // номер месяца с максимальной продажей
+    public int monthMaxSale(int[] sales) { // номер месяца с максимальной продажей
         int month = 0;
         int maxMonth = 0;
         int max = sales[0];
@@ -29,7 +29,7 @@ public class StatsService {
         return maxMonth;
     }
 
-    public static int monthMinSale(int[] sales) { // номер месяца с минимальной продажей
+    public int monthMinSale(int[] sales) { // номер месяца с минимальной продажей
         int month = 0;
         int maxMonth = 0;
         int max = sales[0];
@@ -43,20 +43,22 @@ public class StatsService {
         return maxMonth;
     }
 
-    public static int countMonthSaleLessAverage(int[] sales) { // количество месяцев, где продажа была ниже сренего
+    public int countMonthSaleLessAverage(int[] sales) { // количество месяцев, где продажа была ниже сренего
         int count = 0;
+        int average = average(sales);
         for (int sale : sales){
-            if (sale < StatsService.average(sales)){
+            if (sale < average){
                 count++;
             }
         }
         return count;
     }
 
-    public static int countMonthSaleMoreAverage(int[] sales) { // количество месяцев, где продажа была выше сренего
+    public int countMonthSaleMoreAverage(int[] sales) { // количество месяцев, где продажа была выше сренего
         int count = 0;
+        int average = average(sales);
         for (int sale : sales){
-            if (sale > StatsService.average(sales)){
+            if (sale > average){
                 count++;
             }
         }
--- src/test/java/ru/netology/StatsServiceTest.java
+++ src/test/java/ru/netology/StatsServiceTest.java
@@ -7,49 +7,55 @@ public class StatsServiceTest {
 
     @Test
     void calculateSum(){
+        StatsService service = new StatsService();
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 180;
-        int actual = StatsService.sum(sales);
+        int actual = service.sum(sales);
         assertEquals(expected,actual);
     }
 
     @Test
     void calculateAverage(){
+        StatsService service = new StatsService();
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 15;
-        int actual = StatsService.average(sales);
+        int actual = service.average(sales);
         assertEquals(expected,actual);
     }
 
     @Test
     void calculateMonthMaxSale(){
+        StatsService service = new StatsService();
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 8;
-        int actual = StatsService.monthMaxSale(sales);
+        int actual = service.monthMaxSale(sales);
         assertEquals(expected,actual);
     }
 
     @Test
     void calculateMonthMinSale(){
+        StatsService service = new StatsService();
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 9;
-        int actual = StatsService.monthMinSale(sales);
+        int actual = service.monthMinSale(sales);
         assertEquals(expected,actual);
     }
 
     @Test
     void calculateCountMonthSaleLessAverage(){
+        StatsService service = new StatsService();
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 5;
-        int actual = StatsService.countMonthSaleLessAverage(sales);
+        int actual = service.countMonthSaleLessAverage(sales);
         assertEquals(expected,actual);
     }
 
     @Test
     void calculateCountMonthSaleMoreAverage(){
+        StatsService service = new StatsService();
         int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 5;
-        int actual = StatsService.countMonthSaleMoreAverage(sales);
+        int actual = service.countMonthSaleMoreAverage(sales);
         assertEquals(expected,actual);
     }
 
